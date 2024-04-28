f6802c9aa931ae0cbd6208309483d85b0410d1f8
Pavlov Danil <danpavkzn@mail.ru>
2024-04-28 15:53:57 +0300

    debug-toolbar

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,6 +20,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'debug_toolbar',
+
     'main',
     'goods',
 ]
@@ -32,6 +34,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'app.urls'
@@ -95,5 +99,9 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
